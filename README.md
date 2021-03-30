1574e802e3a2f00973c61ea638404bc4a8284305
Nir Valtman <nir.valtman@finastra.com>
2021-03-29 21:39:32 -0400

    just checking

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ VisualStudio
 ============
 
 OWASP Secure TDD tool for Visual Studio 2013
+
+No longer maintained. 
--- Secure TDD/Secure TDD/Properties/AssemblyInfo.cs
+++ Secure TDD/Secure TDD/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; 
 using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
